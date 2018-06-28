e2831336a59b1ff66c2bcf664278228ec4d58919
lumberjack0101 <39947280+lumberjack0101@users.noreply.github.com>
2018-06-29 05:23:57 +0800

    Add files via upload

--- Universe.py
+++ Universe.py
@@ -1,29 +1,56 @@
 import numpy as np
+import matplotlib.pyplot as mpy
+from SpottiesGame import find_position_from_movement
 
 
 class Universe:
 
-    def __init__(self, lx=10, ly=10, _is_debug=False):
+    def __init__(self, info, lx=10, ly=10, _is_debug=False):
         self._is_debug = _is_debug
         self.size = [lx, ly]
-        self.map_tribe = np.zeros((lx, ly), dtype=int, )
-        self.map_nation = np.zeros((lx, ly), dtype=int, )
+        self.map = dict()
+        self.figure = plot_square_map(lx, ly)
+        for p in info:
+            self.map[p] = np.zeros([lx, ly], dtype=int)
 
     def read_map(self, spotty, length=2):
-        neighbour = {'tribe': np.zeros(1), 'nation': np.zeros(1)}
-        is_nation = check_nation(spotty)
-        neighbour['tribe'] = read_neighbour(self.map_tribe, self.size, spotty, length)
-        if not is_nation == 0:
-            neighbour['nation'] = read_neighbour(self.map_nation, self.size, spotty, length)
-        return neighbour
+        neighbour = dict()
+        for p in spotty.info:
+            neighbour[p] = read_neighbour(self.map[p], self.size, spotty, length)
 
-    def update_universe(self, spotty, move):
-        is_nation = check_nation(spotty)
-        self.map_tribe = update_map(self.map_tribe, spotty.position, spotty.age, spotty.max_age,
-                                    spotty.split_energy, spotty.tribe, move)
-        if not is_nation == 0:
-            self.map_nation = update_map(self.map_nation, spotty.position, spotty.age, spotty.max_age,
-                                         spotty.split_energy, spotty.nation, move)
+    def move_universe(self, spotty, move):
+        self.delete_universe(spotty)
+        spotty.position = find_position_from_movement(spotty.position, move)
+        self.put_universe(spotty)
+        self.figure = self.plot_universe()
+
+    def put_universe(self, spotty):
+        for p in spotty.info:
+            self.map[p][spotty.position[0], spotty.position[1]] = spotty.info[p]
+        self.figure = self.plot_universe()
+
+    def delete_universe(self, spotty):
+        for p in spotty.info:
+            self.map[p][spotty.position[0], spotty.position[1]] = 0
+        self.figure = self.plot_universe()
+
+    def plot_universe(self, _is_show=False):
+        mpy.clf()
+        figure = plot_square_map(self.size[0], self.size[1])
+        occupation = np.zeros([self.size[0], self.size[1]])
+        for p in self.map:
+            occupation = occupation + self.map[p]
+        pos = np.nonzero(occupation)
+        for i in range(0, len(pos[1])):
+            try:
+                map_color = self.map['nation']
+            except KeyError:
+                map_color = np.zeros([self.size[0], self.size[1]])
+            figure = mpy.plot(pos[0][i], pos[1][i], color=list_color()[map_color[pos[0][i], pos[1][i]]],
+                              marker=list_marker()[self.map['tribe'][pos[0][i], pos[1][i]]])
+        if _is_show:
+            mpy.show(figure)
+        return figure
 
 
 def read_nearest_neighbour(the_map, the_size, spotty, cont=5):
@@ -87,29 +114,56 @@ def read_neighbour(the_map, the_size, spotty, length=2):
     return neighbour
 
 
-def check_nation(spotty):
+################################################
+# plot function
+
+
+def plot_square_map(width, height, _is_show=False):
+    figure = mpy.plot()
+    for i in range(0, height):
+        pos0 = [i, i]
+        pos1 = [0, width - 1]
+        figure = mpy.plot(pos0, pos1, 'black')
+    for j in range(0, width):
+        pos0 = [0, height - 1]
+        pos1 = [j, j]
+        mpy.plot(pos0, pos1, 'black')
+    if _is_show:
+        mpy.show()
+    return figure
+
+
+def plot_list():
+    list_plot = {'tribe': list_marker(), 'nation': list_color()}
+    return list_plot
+
+
+def list_color():
+    color_list = ('C1', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9')
+    return color_list
+
+
+def list_marker():
+    marker_list = ('o', 's', 'p', '*', 'h', 'x', 'D')
+    return marker_list
+
+
+# def plot_map(the_map, the_size, the_list):
+#     for i in range(0, the_size[0]):
+#         for j in range(0, the_size[1]):
+#             figure = plot()
+
+
+def plot_put(position, info, _is_show=False):
     try:
-        is_nation = spotty.nation
+        nation = info['nation'] - 1
     except KeyError:
-        is_nation = 0
-    return is_nation
+        nation = 0
+    figure = mpy.plot(position[0], position[1], color=list_color()[nation], marker=list_marker()[info['tribe'] - 1])
+    if _is_show:
+        mpy.show(figure)
+    return figure
 
 
-def update_map(the_map, position, age, max_age, split_energy, info, move):
-    pos_x = position[0]
-    pos_y = position[1]
-    if age == max_age:
-        the_map[pos_x, pos_y] = 0
-    elif not move == 0:
-        the_map[pos_x, pos_y] = 0
-        if split_energy == 1:
-            the_map[pos_x, pos_y] = info
-        if move == 1:
-            the_map[pos_x + 1, pos_y] = info
-        if move == 2:
-            the_map[pos_x, pos_y + 1] = info
-        if move == 3:
-            the_map[pos_x - 1, pos_y] = info
-        if move == 4:
-            the_map[pos_x, pos_y - 1] = info
-    return the_map
+# def plot_delete(position, _is_show=False):
+#     mpy.clf(position)
